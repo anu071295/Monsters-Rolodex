6f5f3c4b2a5c6ed0047984a26b542806147ce159
anu071295 <nanuredha0795@gmail.com>
2020-08-17 08:03:37 -0400

    Updates

---
+++ precache-manifest.cbc13829754a271a48891bb0b30540af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e90dfe367ca08e41c91068728d68e207",
+    "url": "/Monsters-Rolodex/index.html"
+  },
+  {
+    "revision": "1024eb282a540386e9bc",
+    "url": "/Monsters-Rolodex/static/css/main.ab79ffdb.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/Monsters-Rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Monsters-Rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1024eb282a540386e9bc",
+    "url": "/Monsters-Rolodex/static/js/main.3e33efbb.chunk.js"
+  },
+  {
+    "revision": "bb1db29ffdd7a4ded20c",
+    "url": "/Monsters-Rolodex/static/js/runtime-main.f852ebba.js"
+  }
+]);
